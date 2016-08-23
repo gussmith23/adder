0309ed9a1a8e43c822becac17cbe4682cbcba89d
gussmith23 <hfs5022@psu.edu>
2016-08-23 12:53:49 -0400

    convertts to systemverilog

--- testbench.sv
+++ testbench.sv
@@ -1,20 +1,27 @@
+program test(
+  output logic [1:0] a,
+  output logic [1:0] b,
+  input [1:0] y);
+
+  logic [1:0] z;
+
+  initial begin 
+    for (int i = 0; i < 4; i++) begin
+      for (int j = 0; j < 4; j++) begin
+        a = i;
+        b = j;
+        z = i + j;
+        #5
+        assert(y == z);
+        $display("%b + %b == %b", a, b, y);
+      end
+    end
+    $finish;
+  end
+endprogram
+
 module testbench();
-
-reg unsigned [1:0] a, b, y, z;
-
-adder adder(a, b, y);
-
-initial begin 
-	for (int i = 0; i < 4; i++) begin
-		for (int j = 0; j < 4; j++) begin
-			a = i;
-			b = j;
-                        z = i + j;
-                        #5
-			assert(y == z);
-		end
-	end
-        $finish;
-end
-
+  logic [1:0] a, b, y;
+  adder adder(a, b, y);
+  test test_program(a, b, y);
 endmodule
